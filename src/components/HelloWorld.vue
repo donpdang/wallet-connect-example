e7d56c00b726b23f341c995312bb239e6d3a39ae
donpdang <pddang@ucalgary.ca>
2023-04-26 12:47:02 -0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,102 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Trigger a Transaction</h1>
+    <button style="margin-right:12px" @click="connect">Connect</button>
+    <button @click="sendTransaction">Send Transaction</button>
+    <button @click="sendTransactionDelay">Send Transaction delay</button>
   </div>
 </template>
 
 <script>
+import { Web3Provider } from '@ethersproject/providers';
+import EthereumWalletConnectProvider from '@walletconnect/ethereum-provider';
+import {BigNumber} from '@ethersproject/bignumber'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data: () => ({
+    provider: null,
+  }),
+  methods: {
+    async connect() {
+      try {
+        const wcProvider = new EthereumWalletConnectProvider({infuraId: '40604afc9edf4d98a7da2e2ee802c614'});
+        try {
+          await wcProvider.enable();
+          window.walletProvider = new Web3Provider(wcProvider, 5);
+        } catch (e) {
+          console.error(e);
+        }
+      } catch (error) {
+        alert(`Error: ${error.message}`);
+      }
+    },
+    async sendTransaction() {
+      try {
+        const signer = window.walletProvider.getSigner();
+        const toAddress = '0xCD56df7B4705A99eBEBE2216e350638a1582bEC4'; // Replace with the actual recipient's address
+        const value = BigNumber.from(0);
+
+        const transaction = {
+          to: toAddress,
+          value: value,
+          gasLimit: 12467
+        };
+
+
+        const tx = await signer.sendUncheckedTransaction(transaction);
+        alert(`Transaction sent: ${tx.hash}`);
+      } catch (error) {
+        alert(`Error: ${error.message}`);
+      }
+    },
+    async sendTransactionDelay() {
+
+    try {
+      const wcProvider = new EthereumWalletConnectProvider({infuraId: '40604afc9edf4d98a7da2e2ee802c614'});
+      let provider
+      try {
+        await wcProvider.enable();
+        provider = new Web3Provider(wcProvider, 5);
+      } catch (e) {
+        console.error(e);
+      }
+      const signer = provider.getSigner();
+      const toAddress = '0xCD56df7B4705A99eBEBE2216e350638a1582bEC4'; // Replace with the actual recipient's address
+      const value = BigNumber.from(0);
+
+      const transaction = {
+        to: toAddress,
+        value: value,
+      };
+
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const tx = await signer.sendTransaction(transaction);
+      alert(`Transaction sent: ${tx.hash}`);
+    } catch (error) {
+      alert(`Error: ${error.message}`);
+    }
+},
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1,
+h2 {
+  font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import { Buffer } from 'buffer';
+window.Buffer = window.Buffer || Buffer;
 createApp(App).mount('#app')
